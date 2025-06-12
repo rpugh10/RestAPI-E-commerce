c70df37d5f4bd77be8abb5af43312b8306a45242
Ryan Pugh <74481946+rpugh10@users.noreply.github.com>
2025-06-12 16:35:07 -0700

    Add files via upload

---
+++ Message.java
@@ -0,0 +1,13 @@
+package com.codewithmosh.store.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Message {
+
+    private String text;
+
+
+}
