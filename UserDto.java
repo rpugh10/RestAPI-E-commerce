6337afc102e20c250a3415018ba25fcf2e393dea
Ryan Pugh <74481946+rpugh10@users.noreply.github.com>
2025-06-16 23:31:02 -0700

    Add files via upload

--- UserDto.java
+++ UserDto.java
@@ -6,7 +6,13 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public class UserDto {
+   // @JsonIgnore Use this if there is other code that depends on this code
+   // @JsonProperty("user_id") changes the name of the table to what is passed in the parameter
     public Long id;
     public String name;
     public String email;
+    // @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss") Formats the time
+    // private LocalDateTime createAt; Created a variable to display in the UserMapper class
+    // @JsonInclude(JsonInclude.Include.NON_NULL) if the data is null the table will be ignored and not displayed
+     // private String phoneNumber; The users do not have a phone number is this api
 }
