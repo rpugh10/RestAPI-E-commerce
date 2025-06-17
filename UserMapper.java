4c3e54429a4633aec58bbd46cec2b8b25fe7e72f
Ryan Pugh <74481946+rpugh10@users.noreply.github.com>
2025-06-16 23:31:17 -0700

    Add files via upload

--- UserMapper.java
+++ UserMapper.java
@@ -9,5 +9,6 @@ import com.codewithmosh.store.entities.User;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
+    // @Mapping(target = "createAt", expression = "java(java.time.LocalDateTime.now())")
     UserDto toDto(User user);
 }
