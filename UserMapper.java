64933a0eb01e52a308974af1418ee7edbd114845
Ryan Pugh <74481946+rpugh10@users.noreply.github.com>
2025-06-19 18:42:35 -0700

    Add files via upload

--- UserMapper.java
+++ UserMapper.java
@@ -3,8 +3,10 @@ package com.codewithmosh.store.mappers;
 
 
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 
 import com.codewithmosh.store.dtos.RegisterUserDto;
+import com.codewithmosh.store.dtos.UpdateUserRequest;
 import com.codewithmosh.store.dtos.UserDto;
 import com.codewithmosh.store.entities.User;
 
@@ -13,4 +15,5 @@ public interface UserMapper {
     // @Mapping(target = "createAt", expression = "java(java.time.LocalDateTime.now())")
     UserDto toDto(User user);
     User toEntity(RegisterUserDto registerUserDto);
+    void update(UpdateUserRequest request,@MappingTarget User user);
 }
